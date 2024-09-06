3e9e5bfcc01da71597a107d5dd9af98263b6c572
David Alvarez <a.21308051280310@cbtis128.edu.mx>
2024-09-06 09:42:46 -0600

    Add files via upload
    archivos del codigo

---
+++ miscadenas.py
@@ -0,0 +1,16 @@
+a='''David, 
+trabajando con cadenas
+boo'''
+
+print("otro ejemplo")
+#  0123456
+b="Alvarez"
+print(b[2:5])
+print("a mayusculas")
+a="Alvarez"
+print(a.upper())
+print("Concatenar")
+a="Maria"
+b="Perez"
+c=a+" "+b
+print(c)
---
+++ misvariables.py
@@ -0,0 +1,12 @@
+import math # imports the math library
+eda=69 # se guarda en eda
+NOMbre="David Alvarez"
+print("Tu edad es:", eda, "\nTe llamas", NOMbre)
+print("-- tipo de variables --")
+x=str(3) # x will be '3'
+y=int(3.9) # y will be 3
+z=float(3) # z will be 3.0
+print("",x,"\n",y,"\n",z)
+print("-- funciones matematicas --")
+x=math.sqrt(64) # pulls the sqrt function from the math library. sqrt is square root of (64) which is 8.
+print(x) 
